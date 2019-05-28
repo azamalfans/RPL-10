d401341672e99f7871584e14e14a5a89d78494ec
Faishal muhammad <50820698+faishalmohammed@users.noreply.github.com>
2019-05-28 14:52:12 +0700

    Add files via upload

---
+++ include/header.php
@@ -0,0 +1,8 @@
+<div id="container">
+  <header>
+    <div class="width">
+      <img class="logo" src="image/tutwur.png">
+      <h1><a href="?page=admin">SMA <span>NEGERI</span> 99</a></h1>
+      <h2>Sistem Informasi Absensi SMAN 99</h2>
+    </div>
+  </header>
---
+++ include/menu.php
@@ -0,0 +1,10 @@
+<nav>
+    <div class="width">
+      <ul>
+          <li><a href="beranda.php">Beranda</a></li>
+          <li><a href="profil.php">Profil</a></li>
+          <li><a href="tentang.php">Tentang</a></li>
+          <li><a href="datasiswa.php">Data Absensi Siswa</a></li>
+      </ul>
+    </div>
+  </nav>
