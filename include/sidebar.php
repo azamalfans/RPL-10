83ac5f353bb4f1f1eb10e4f84439627ba176c3a9
Faishal muhammad <50820698+faishalmohammed@users.noreply.github.com>
2019-05-28 14:52:49 +0700

    Add files via upload

---
+++ include/sidebar.php
@@ -0,0 +1,17 @@
+<aside class="sidebar">
+            <ul>  
+               <li>
+                    <h4>Selamat Datang</h4>
+                    <ul>
+                        Selamat Datang di Website Sistem Informasi Absensi SMA Negeri 99
+                    </ul>
+                </li>
+                
+                <li>
+                    <h4>Tentang kami</h4>
+                    <ul>
+                        Kunjungi Website sekolah kami di <a href="#">www.sman99.sch.com</a>
+                    </ul>
+                </li>
+            </ul>
+</aside>
---
+++ include/sidebarinfo.php
@@ -0,0 +1,15 @@
+<aside>            
+        <li>
+            <h4><i class="fa fa-info-circle fa"></i> Info</h4>
+            <ul>
+                Gunakan Dashboard untuk mempercepat proses absensi.
+            </ul>
+        </li>
+
+        <li>
+            <h4><i class="fa fa-warning fa"></i> Peringatan</h4>
+            <ul>
+                Jangan lupa untuk backup laporan secara rutin untuk menghindari kehilangan data
+            </ul>
+        </li>
+</aside>
