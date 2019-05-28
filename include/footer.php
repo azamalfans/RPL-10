3a19e0ad93caad148b34bb6693f74f27d17b993a
Faishal muhammad <50820698+faishalmohammed@users.noreply.github.com>
2019-05-28 14:53:15 +0700

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,29 @@
+<div class="clear"></div>
+    </div>
+    <footer>
+        <div class="footer-content width">
+            <ul>
+                <li><h4>Kontak Kami</h4></li>
+                <p><i class="fa fa-phone fa"></i> 0224-12345678</a></li>
+                <p><i class="fa fa-book"></i> sman99@sch.com</a></li>
+                <p><i class="fa fa-fax fa"></i> 123-456789</a></li>
+            </ul>
+            
+            <ul>
+              <li><h4>Alamat</h4></li>
+                <p><i class="fa fa-map-marker fa"></i> Jl.inajadulu Rt.10 Rw.10 No.99 Kelurahan Sekaran Kecamatan Gunungpati</a></li>
+            </ul>
+            
+            <ul class="endfooter">
+              <li><h4>Sosial Media</h4></li>
+                <li><a href="#"><i class="fa fa-instagram fa"></i> Instagram</a></li>
+                <li><a href="#"><i class="fa fa-facebook-square fa"></i> Facebook</a></li>
+                <li><a href="#"><i class="fa fa-twitter fa"></i> Twitter</a></li>
+            </ul>
+            
+            <div class="clear"></div>
+        </div>
+        <div class="footer-bottom">
+            <p>Hak Cipta 2019 &copy; <strong>SMA Negeri 99 2019.</strong></p>
+         </div>
+    </footer>
